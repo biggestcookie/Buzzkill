c5cdd2d1db7175bca9ce8576b5aaa7bd3dfe9ee5
Paul Hansa <bigcooki3@users.noreply.github.com>
2018-10-06 23:39:20 -0400

    First commit of site files

---
+++ js/comments.js
@@ -0,0 +1,121 @@
+
+
+var langs =
+    [['Afrikaans', ['af-ZA']],
+    ['Bahasa Indonesia', ['id-ID']],
+    ['Bahasa Melayu', ['ms-MY']],
+    ['Català', ['ca-ES']],
+    ['Čeština', ['cs-CZ']],
+    ['Deutsch', ['de-DE']],
+    ['English', ['en-AU', 'Australia'],
+        ['en-CA', 'Canada'],
+        ['en-IN', 'India'],
+        ['en-NZ', 'New Zealand'],
+        ['en-ZA', 'South Africa'],
+        ['en-GB', 'United Kingdom'],
+        ['en-US', 'United States']],
+    ['Español', ['es-AR', 'Argentina'],
+        ['es-BO', 'Bolivia'],
+        ['es-CL', 'Chile'],
+        ['es-CO', 'Colombia'],
+        ['es-CR', 'Costa Rica'],
+        ['es-EC', 'Ecuador'],
+        ['es-SV', 'El Salvador'],
+        ['es-ES', 'España'],
+        ['es-US', 'Estados Unidos'],
+        ['es-GT', 'Guatemala'],
+        ['es-HN', 'Honduras'],
+        ['es-MX', 'México'],
+        ['es-NI', 'Nicaragua'],
+        ['es-PA', 'Panamá'],
+        ['es-PY', 'Paraguay'],
+        ['es-PE', 'Perú'],
+        ['es-PR', 'Puerto Rico'],
+        ['es-DO', 'República Dominicana'],
+        ['es-UY', 'Uruguay'],
+        ['es-VE', 'Venezuela']],
+    ['Euskara', ['eu-ES']],
+    ['Français', ['fr-FR']],
+    ['Galego', ['gl-ES']],
+    ['Hrvatski', ['hr_HR']],
+    ['IsiZulu', ['zu-ZA']],
+    ['Íslenska', ['is-IS']],
+    ['Italiano', ['it-IT', 'Italia'],
+        ['it-CH', 'Svizzera']],
+    ['Magyar', ['hu-HU']],
+    ['Nederlands', ['nl-NL']],
+    ['Norsk bokmål', ['nb-NO']],
+    ['Polski', ['pl-PL']],
+    ['Português', ['pt-BR', 'Brasil'],
+        ['pt-PT', 'Portugal']],
+    ['Română', ['ro-RO']],
+    ['Slovenčina', ['sk-SK']],
+    ['Suomi', ['fi-FI']],
+    ['Svenska', ['sv-SE']],
+    ['Türkçe', ['tr-TR']],
+    ['български', ['bg-BG']],
+    ['Pусский', ['ru-RU']],
+    ['Српски', ['sr-RS']],
+    ['한국어', ['ko-KR']],
+    ['中文', ['cmn-Hans-CN', '普通话 (中国大陆)'],
+        ['cmn-Hans-HK', '普通话 (香港)'],
+        ['cmn-Hant-TW', '中文 (台灣)'],
+        ['yue-Hant-HK', '粵語 (香港)']],
+    ['日本語', ['ja-JP']],
+    ['Lingua latīna', ['la']]];
+
+function updateCountry() {
+    for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+        select_dialect.remove(i);
+    }
+    var list = langs[select_language.selectedIndex];
+    for (var i = 1; i < list.length; i++) {
+        select_dialect.options.add(new Option(list[i][1], list[i][0]));
+    }
+    select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+}
+
+function createEmail() {
+    var n = final_transcript.indexOf('\n');
+    if (n < 0 || n >= 80) {
+        n = 40 + final_transcript.substring(40).indexOf(' ');
+    }
+    var subject = encodeURI(final_transcript.substring(0, n));
+    var body = encodeURI(final_transcript.substring(n + 1));
+    window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
+}
+
+function copyButton() {
+    if (recognizing) {
+        recognizing = false;
+        recognition.stop();
+    }
+    copy_button.style.display = 'none';
+    copy_info.style.display = 'inline-block';
+    showInfo('');
+}
+
+function emailButton() {
+    if (recognizing) {
+        create_email = true;
+        recognizing = false;
+        recognition.stop();
+    } else {
+        createEmail();
+    }
+    email_button.style.display = 'none';
+    email_info.style.display = 'inline-block';
+    showInfo('');
+}
+
+
+function showButtons(style) {
+    if (style == current_style) {
+        return;
+    }
+    current_style = style;
+    copy_button.style.display = style;
+    email_button.style.display = style;
+    copy_info.style.display = 'none';
+    email_info.style.display = 'none';
+}
---
+++ js/script.js
@@ -0,0 +1,265 @@
+function returnMap(wordList) {
+    let wordMap = new Map();
+    for (i = 0; wordList.length > i; i++) {
+        wordMap.set(wordList[i], i);
+    }
+    return wordMap;
+}
+
+function capitalize(s) {
+    return s.replace(first_char, function (m) { return m.toUpperCase(); });
+}
+
+function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+function upgrade() {
+    start_button.style.visibility = 'hidden';
+    showInfo('info_upgrade');
+}
+
+function showInfo(s) {
+    if (s) {
+        for (var child = info.firstChild; child; child = child.nextSibling) {
+            if (child.style) {
+                child.style.display = child.id == s ? 'inline' : 'none';
+            }
+        }
+        info.style.visibility = 'visible';
+    } else {
+        info.style.visibility = 'hidden';
+    }
+}
+
+function startButton(event) {
+    if (recognizing) {
+        recognition.stop();
+        return;
+    }
+    final_transcript = '';
+    recognition.lang = select_dialect.value;
+    recognition.start();
+    ignore_onend = false;
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+    start_img.src = './img/mic-slash.gif';
+    showInfo('info_allow');
+    // showButtons('none');
+    start_timestamp = event.timeStamp;
+}
+
+var score = 0;
+
+words = [
+    "adaptive",
+    "agile",
+    "ai",
+    "ajax",
+    "algorithm",
+    "android",
+    "analytics",
+    "api",
+    "artificial intelligence",
+    "augmented",
+    "authentication",
+    "big data",
+    "biometric",
+    "bitcoin",
+    "blockchain",
+    "bot",
+    "botnet",
+    "bootstrap",
+    "cloud",
+    "cloud-based",
+    "cross-platform",
+    "cyber",
+    "data mining",
+    "dark web",
+    "deep web",
+    "devops",
+    "dmca",
+    "encryption",
+    "encrypted",
+    "end-to-end",
+    "emacs",
+    "framework",
+    "functional",
+    "gdpr",
+    "hack",
+    "hackathon",
+    "hacker",
+    "hacking",
+    "internet of things",
+    "ios",
+    "iot",
+    "linux",
+    "machine learning",
+    "ml",
+    "mobile",
+    "multi-threading",
+    "multi-core",
+    "neural network",
+    "network",
+    "networking",
+    "predictive",
+    "polymorphism",
+    "quantum",
+    "referential transparency",
+    "responsive",
+    "rest",
+    "scalability",
+    "science",
+    "scrum",
+    "security",
+    "serverless",
+    "skeuomorphic",
+    "software",
+    "tape drive",
+    "threat management",
+    "tor",
+    "tuples",
+    "turing complete",
+    "ubuntu",
+    "unit test",
+    "unix",
+    "virtual reality",
+    "vim",
+    "vr",
+    "waterfall"
+]
+
+let wordMap = returnMap(words);
+function inMap(wordMap, word) {
+    word = word.toLowerCase();
+    return (wordMap.get(word) > -1);
+};
+
+select_language = 'English';
+select_dialect = 'en-US';
+showInfo('info_start');
+
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;
+
+
+if (!('webkitSpeechRecognition' in window)) {
+    upgrade();
+} else {
+    start_button.style.display = 'inline-block';
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = function () {
+        recognizing = true;
+        showInfo('info_speak_now');
+        start_img.src = '../img/mic-animate.gif';
+        score = 0;
+    };
+
+    recognition.onerror = function (event) {
+        if (event.error == 'no-speech') {
+            start_img.src = '../img/mic.gif';
+            showInfo('info_no_speech');
+            ignore_onend = true;
+        }
+        if (event.error == 'audio-capture') {
+            start_img.src = '../img/mic.gif';
+            showInfo('info_no_microphone');
+            ignore_onend = true;
+        }
+        if (event.error == 'not-allowed') {
+            if (event.timeStamp - start_timestamp < 100) {
+                showInfo('info_blocked');
+            } else {
+                showInfo('info_denied');
+            }
+            ignore_onend = true;
+        }
+    };
+
+    recognition.onend = function () {
+        recognizing = false;
+        if (ignore_onend) {
+            return;
+        }
+        start_img.src = './img/mic.gif';
+        if (!final_transcript) {
+            showInfo('info_start');
+            return;
+        }
+        showInfo('');
+        if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+            var range = document.createRange();
+            range.selectNode(document.getElementById('final_span'));
+            window.getSelection().addRange(range);
+        }
+    };
+
+    recognition.onresult = function (event) {
+        var interim_transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+                score = calculateScoreLive(score, event.results[i][0].transcript);
+                console.log("Live score: " + score);
+            }
+        }
+        final_transcript = capitalize(final_transcript);
+        var final_score = calculateScoreFinalized(final_transcript);
+        console.log("Final score: " + final_score);
+        final_span.innerHTML = linebreak(final_transcript);
+        interim_span.innerHTML = linebreak(interim_transcript);
+    };
+}
+
+function calculateScoreLive(score, transcript) {
+    var words = transcript.split(" ");
+    console.log('Checking: ' + words[words.length - 1]);
+    if (inMap(wordMap, words[words.length - 1])) {
+        score++;
+        return score;
+    }
+    return score;
+};
+
+function calculateScoreFinalized(final_transcript) {
+    // Our longest word phrase in the dictionary is 3 words. So we will look at
+    // up to 2 words before and 2 words after to see if it matches anything.
+    var words = final_transcript.split(" ");
+    var final_score = 0;
+    for (index in words) {
+        try {
+            if (inMap(wordMap, words[index])) {
+                final_score++;
+            } else if (inMap(wordMap, words[index - 1] + " " + words[index])) { // 1 word before
+                final_score++;
+            } else if (inMap(wordMap, words[index - 2] + " " + words[index - 1] + " " + words[index])) {// 2 words before
+                final_score++;
+            } else if (inMap(wordMap, words[index] + " " + words[index + 1])) { // 1 word after
+                final_score++;
+            } else if (inMap(wordMap, words[index] + " " + words[index + 1] + " " + words[index + 2])) { // 2 words after
+                final_score++;
+            }
+        } catch (err) {
+            // do nothing.
+            console.log(err);
+        }
+    }
+    return final_score;
+};
+
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+
+
+var first_char = /\S/;
+
+
+var current_style;
